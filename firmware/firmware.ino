a69bcced72f9e3e056435cfeda717799f1ef9e2b
Brandon Satrom <bsatrom+gh@gmail.com>
2020-12-28 14:41:14 -0600

    add firmware

---
+++ firmware/firmware.ino
@@ -0,0 +1,19 @@
+
+// Include the Arduino library for the Notecard
+#include <Notecard.h>
+#include <Wire.h>
+
+#define serialDebugOut Serial
+
+#define myProductID "com.blues.bsatrom:build_monitor"
+Notecard notecard;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
